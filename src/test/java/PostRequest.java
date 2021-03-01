75c33c747ae8ec7a8c1f747f294f07b72fdbcabb
Shurkovalina <shurkowalina@yandex.ru>
2021-03-01 20:52:08 +0300

    added files

---
+++ src/test/java/PostRequest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostRequest {
+
+    @Test
+    public void postRequest(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Bonne nuit")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("\"Bonne nuit\""))
+        ;
+    }
+}
